50532815f4ae1fc90f4d53326c3307f1d899b4c5
Daan Middendorp <github-d@landgenoot.com>
2019-10-03 01:50:53 +0200

    Move JS to seprate file

---
+++ initial-talk/js/tu-berlin.js
@@ -0,0 +1,32 @@
+
+Reveal.initialize({
+  dependencies: [
+    { src: 'plugin/markdown/marked.js' },
+    { src: 'plugin/markdown/markdown.js' },
+    { src: 'plugin/notes/notes.js', async: true },
+    { src: 'plugin/highlight/highlight.js', async: true }
+  ]
+})
+
+var header = $('#header').html();
+if (window.location.search.match(/print-pdf/gi)) {
+  Reveal.addEventListener('ready', function (event) {
+    $('.slide-background').append(header)
+  })
+} else {
+  $('div.reveal').append(header)
+  $('#footer-left').hide()
+  $('#footer-right').hide()
+}
+
+Reveal.addEventListener('slidechanged', (event) => {
+  const isFirst = event.indexh === 0;
+  Reveal.configure({ slideNumber: !isFirst });
+  if (isFirst) {
+    $('#footer-right').hide()
+    $('#footer-left').hide()
+  } else {
+    $('#footer-right').show()
+    $('#footer-left').show()
+  }
+});
